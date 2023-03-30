01a4af53c760415bce3b4cc8a6bfb99b72cf59fd
Nick Elobsky <echonok@gmail.com>
2023-03-30 11:35:07 +0200

    added some componment

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import ToDoList from './components/ToDoList';
+import NewToDo from './components/NewToDo';
 
 const App: React.FC = () => {
+  const todos = [{ id: 't1', text: 'Finish it' }];
   return (
     <div className="App">
+      <NewToDo />
+      <ToDoList items={todos} />
     </div>
   );
 }
